fa29056986fa8a7fc1f76affb85933a408797be7
Junhyeokss <junhyeok0516@naver.com>
2023-02-27 15:26:51 +0900

    2023-02-27-sh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+// 1. api : db가 있음
+// 2. db에서 data를 가져옴 useEffect 데이터 가져올때 React
+// 3. 가져온 data를 React로 뿌려줌
+// 4.
+
+const UL = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  gap: 10px;
+  img {
+    max-width: 100%;
+  }
+  @media (max-width: 768px) {
+    grid-templete-columns: repeat (1, 1fr);
+  }
+`;
+
+const H1 = styled.h1`
+  text-align: center;
+`;
+
+const Search = styled.div`
+  text-align: center;
+  padding: 20px 0;
+`;
 
 function App() {
+  const [pic, sepic] = useState([]);
+  const [s, setS] = useState();
+  const getData = async () => {
+    const data = await fetch(
+      `https://pixabay.com/api/?key=21103852-9b5f4834542caaf4eef2c8533&q=${s}&image_type=photo`
+    ).then((r) => r.json());
+
+    //console.log(data, data.hits, data.hits[0].id);
+    sepic(data.hits);
+    console.log(data.hits);
+  };
+
+  // useEffect는 한번만 실행하는 변수
+  useEffect(() => {}, []);
+
+  getData();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <H1>김준혁의 첫번째 리엑트</H1>
+      <Search>
+        <div>
+          search :{" "}
+          <input onChange={(e) => setS(e.target.value)} value={s}></input>
+        </div>
+      </Search>
+      <UL>
+        {pic.map((it) => {
+          return (
+            <li key={it.id}>
+              <img src={it.largeImageURL} />
+              <div>{it.id}</div>
+              <div>{it.tags}</div>
+            </li>
+          );
+        })}
+      </UL>
+    </>
   );
 }
 
